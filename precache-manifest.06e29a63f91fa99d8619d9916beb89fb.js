a94c4c8aa35c7eb59f8a8601419737fa961c8cc6
Alena Gusakov <agusakov@udel.edu>
2020-08-15 16:35:41 -0400

    Updates

---
+++ precache-manifest.06e29a63f91fa99d8619d9916beb89fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc5f97f0ae53726e38d2a5d1f4f7b31",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0b143836b4ac288829ac",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5ca66d7c11392dfacbb",
+    "url": "/react-gh-pages/static/js/2.aa2e5d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.aa2e5d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b143836b4ac288829ac",
+    "url": "/react-gh-pages/static/js/main.69715f82.chunk.js"
+  },
+  {
+    "revision": "d90a9efe7b1047e97d0d",
+    "url": "/react-gh-pages/static/js/runtime-main.d0a9d1e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
