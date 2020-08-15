cccb18fb708411d430c42aee4b727522c418e808
Alena Gusakov <agusakov@udel.edu>
2020-08-15 16:51:31 -0400

    Updates

---
+++ precache-manifest.2baa3182e9ed31d4c4f294037ecebf98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03590a5a1a93b27a45eeeed259753349",
+    "url": "/professional-site/index.html"
+  },
+  {
+    "revision": "2db77995607a88a12589",
+    "url": "/professional-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5ca66d7c11392dfacbb",
+    "url": "/professional-site/static/js/2.aa2e5d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/professional-site/static/js/2.aa2e5d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db77995607a88a12589",
+    "url": "/professional-site/static/js/main.81f2be43.chunk.js"
+  },
+  {
+    "revision": "c1a15786c617d53cf846",
+    "url": "/professional-site/static/js/runtime-main.e4eb443a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/professional-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.06e29a63f91fa99d8619d9916beb89fb.js"
+  "/professional-site/precache-manifest.2baa3182e9ed31d4c4f294037ecebf98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/professional-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
